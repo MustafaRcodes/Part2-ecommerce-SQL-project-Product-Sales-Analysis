caa929ad7f0e40725da739847d55b7692032a01f
MustafaR_Projects <150495517+MustafaRcodes@users.noreply.github.com>
2024-07-18 00:32:23 -0400

    Add files via upload

--- Product analysis.sql
+++ Product analysis.sql
@@ -333,9 +333,186 @@ FROM orders
 WHERE orders.order_id BETWEEN 10000 AND 11000
 GROUP BY orders.primary_product_id;
 
+
+
 -- On september 25th we started giving customers the option to add a 2nd product while on the /cart page.
 -- We need to compare the month before vs the month after the change. we would like to see CTR from the
 -- /cart page, avg product per order, AOV, and overall revenue per/cart page view. 
 
+-- CROSS SALES ANALYSIS -- 
+-- Step 1: Identify the relevant /cart page view and their sessions
+-- Step 2: See which of those /cart sessions clicked through to the shipping page.
+-- Step 3: Find the orders associated with the /cart sessions. Analyze products purchased, AOV
+-- Step 4: Aggregate and analyze a summary of our finding.
+
+CREATE TEMPORARY TABLE sessions_seeing_cart
+SELECT 
+   CASE 
+   WHEN created_at < '2013-09-25' THEN  'A. Pre_Cross_Sell'
+   WHEN created_at >= '2013-01-06' THEN 'B. Post_Cross_Sell'
+   ELSE 'check logic'
+   END AS time_period,
+     website_session_id AS  cart_session_id,
+     website_pageview_id AS cart_pageview_id
+FROM website_pageviews
+WHERE created_at BETWEEN '2013-08-25' AND '2013-10-25'
+    AND pageview_url = '/cart';
+    
+CREATE TEMPORARY TABLE cart_sessions_seeing_another_page
+SELECT 
+   sessions_seeing_cart.time_period,
+   sessions_seeing_cart.cart_session_id,
+   MIN(website_pageviews.website_pageview_id) AS pv_id_after_cart
+FROM sessions_seeing_cart
+   LEFT JOIN website_pageviews
+     ON website_pageviews.website_session_id = sessions_seeing_cart.cart_session_id
+     AND website_pageviews.website_pageview_id > sessions_seeing_cart.cart_pageview_id
+GROUP BY 
+    sessions_seeing_cart.time_period,
+    sessions_seeing_cart.cart_session_id
+HAVING 
+     MIN(website_pageviews.website_pageview_id) IS NOT NULL;
+     
+CREATE TEMPORARY TABLE pre_post_sessions_orders 
+SELECT 
+    time_period,
+    cart_session_id,
+    order_id,
+    items_purchased,
+    price_usd
+    
+FROM  sessions_seeing_cart
+     INNER JOIN orders
+        ON sessions_seeing_cart.cart_session_id = orders.website_session_id;
+        
+-- first, we'll look at this select statement 
+-- then we'll turn it into a subquery
+
+SELECT 
+    sessions_seeing_cart.time_period,
+    sessions_seeing_cart.cart_session_id,
+    CASE WHEN cart_sessions_seeing_another_page.cart_session_id IS NULL THEN 0 ELSE 1 END AS clicked_to_another_page,
+    CASE WHEN pre_post_sessions_orders.order_id IS NULL THEN 0 ELSE 1 END AS place_order,
+    pre_post_sessions_orders.items_purchased,
+    pre_post_sessions_orders.price_usd
+FROM sessions_seeing_cart
+	LEFT JOIN cart_sessions_seeing_another_page
+       ON sessions_seeing_cart.cart_session_id = cart_sessions_seeing_another_page.cart_session_id
+	LEFT JOIN pre_post_sessions_orders
+       ON sessions_seeing_cart.cart_session_id = pre_post_sessions_orders.cart_session_id
+ORDER BY cart_session_id
+;
+    
+ SELECT 
+    time_period,
+    COUNT(DISTINCT cart_session_id) AS cart_sessions,
+    SUM(clicked_to_another_page) AS clickthroughs,
+    SUM(clicked_to_another_page)/COUNT(DISTINCT cart_session_id) AS cart_ctr,
+    -- SUM(placed_order) AS orders_placed,
+    -- SUM(items_purchased) AS products_purchased,
+    SUM(items_purchased)/SUM(placed_order) AS products_per_order,
+    -- SUM(price_usd) AS revenue,
+    SUM(price_usd)/SUM(placed_order) AS aov,
+    SUM(price_usd)/COUNT(DISTINCT cart_session_id) AS rev_per_cart_session
+FROM(
+SELECT 
+    sessions_seeing_cart.time_period,
+    sessions_seeing_cart.cart_session_id,
+    CASE WHEN cart_sessions_seeing_another_page.cart_session_id IS NULL THEN 0 ELSE 1 END AS clicked_to_another_page,
+    CASE WHEN pre_post_sessions_orders.order_id IS NULL THEN 0 ELSE 1 END AS placed_order,
+    pre_post_sessions_orders.items_purchased,
+    pre_post_sessions_orders.price_usd
+FROM sessions_seeing_cart
+	LEFT JOIN cart_sessions_seeing_another_page
+       ON sessions_seeing_cart.cart_session_id = cart_sessions_seeing_another_page.cart_session_id
+	LEFT JOIN pre_post_sessions_orders
+       ON sessions_seeing_cart.cart_session_id = pre_post_sessions_orders.cart_session_id
+ORDER BY 
+     cart_session_id
+) AS full_data
+GROUP BY time_period;
+
+-- It looks like the CTR from the /cart page didn't go down and that our product per order, AOV and 
+-- revenue per/cart session are all up slightly since the cross-sell feature was added.
 
 
+-- PRODUCT PORTFOLIO EXPANSION --
+-- will run a pre-post analysis comparing the month before vs the month after, in terms of session-to-order
+-- conversion rate, AOV, product per order and revenue per session.
+
+SELECT 
+   CASE 
+      WHEN website_sessions.created_at < '2013-12-12' THEN 'A. Pre_Birthday_Bear'
+      WHEN website_sessions.created_at >= '2013-12-12' THEN 'B. Post_Birthday_Bear'
+      ELSE 'check logic'
+END AS time_period,
+COUNT(DISTINCT website_sessions.website_session_id) AS sessions,
+COUNT(DISTINCT orders.order_id) AS orders,
+COUNT(DISTINCT orders.order_id)/COUNT(DISTINCT website_sessions.website_session_id) AS conv_rate,
+SUM(orders.price_usd) AS total_revenue,
+SUM(orders.items_purchased) AS total_product_sold,
+SUM(orders.price_usd)/COUNT(DISTINCT orders.order_id) AS average_order_value,
+SUM(orders.items_purchased)/COUNT(DISTINCT orders.order_id) AS products_per_order,
+SUM(orders.price_usd)/COUNT(DISTINCT website_sessions.website_session_id) AS revenue_per_session
+FROM website_sessions
+   LEFT JOIN orders
+     ON orders.website_session_id = website_sessions.website_session_id
+WHERE website_sessions.created_at BETWEEN '2013-11-12' AND '2014-01-12'
+GROUP BY 1;
+
+-- It looks like all of our critical metrics have improved since we launched the third product.
+-- we may consider adding a fourth product.
+
+
+-- PRODUCT REFUND ANALYSIS -- 
+/* Analyzing product refund rates is about controlling for quanlity and understanding where 
+we might have problems to address.
+Keeping a eye on refund rates is a great way to analyze the relative quality of product, track customer
+satisfaction and to undertand overall business health.
+- Monitoring products from different suppliers.
+- Undertanding refund rates for products at different price point.
+- Taking product refund rates and the associated costs into account when assessing the overall perfromance
+of our business.
+*/
+SELECT 
+    order_items.order_id,
+    order_items.order_item_id,
+    order_items.price_usd AS price_paid_usd,
+    order_items.created_at,
+    order_item_refunds.order_item_refund_id,
+    order_item_refunds.refund_amount_usd,
+    order_item_refunds.created_at
+FROM order_items
+    LEFT JOIN order_item_refunds
+        ON order_item_refunds.order_item_id = order_items.order_item_id
+WHERE order_items.order_id IN (3489,32049,27061);
+
+-- Supplier has some quality issues which weren't corrected until september 2013.
+-- Major problem is bear's arms were falling off in Aug/Sep 2014. as a result we replaced it with a new 
+-- supplier on September 16, 2014
+-- We need to pull monthly product refund rates by product, and confirm  our quanlity issues are now fixed.alter
+
+SELECT 
+   YEAR(order_items.created_at) AS yr,
+   MONTH(order_items.created_at) AS mo,
+   COUNT(DISTINCT CASE WHEN product_id = 1 THEN order_items.order_item_id ELSE NULL END) AS p1_orders,
+   COUNT(DISTINCT CASE WHEN product_id = 1 THEN order_item_refunds.order_item_id ELSE NULL END)
+       /COUNT(DISTINCT CASE WHEN product_id = 1 THEN order_items.order_item_id ELSE NULL END ) AS p1_refund_rt,
+   COUNT(DISTINCT CASE WHEN product_id = 2 THEN order_items.order_item_id ELSE NULL END) AS p2_orders,
+   COUNT(DISTINCT CASE WHEN product_id = 2 THEN order_item_refunds.order_item_id ELSE NULL END)
+       /COUNT(DISTINCT CASE WHEN product_id = 2 THEN order_items.order_item_id ELSE NULL END ) AS p2_refund_rt,
+   COUNT(DISTINCT CASE WHEN product_id = 3 THEN order_items.order_item_id ELSE NULL END) AS p3_orders,
+   COUNT(DISTINCT CASE WHEN product_id = 3 THEN order_item_refunds.order_item_id ELSE NULL END)
+       /COUNT(DISTINCT CASE WHEN product_id = 3 THEN order_items.order_item_id ELSE NULL END ) AS p3_refund_rt,
+   COUNT(DISTINCT CASE WHEN product_id = 4 THEN order_items.order_item_id ELSE NULL END) AS p4_orders,
+   COUNT(DISTINCT CASE WHEN product_id = 4 THEN order_item_refunds.order_item_id ELSE NULL END)
+       /COUNT(DISTINCT CASE WHEN product_id = 4 THEN order_items.order_item_id ELSE NULL END ) AS p4_refund_rt
+FROM order_items
+	LEFT JOIN order_item_refunds
+       ON order_items.order_item_id = order_item_refunds.order_item_id
+WHERE order_items.created_at < '2014-10-15'
+GROUP BY 1,2;
+
+-- Looks like the refund rates did go down after the initial improvements in september 2013,
+-- but refund rate were terrible in August and September, as expected 13-14 %.
+-- Seems like the new supplier is doing much better and the other products looks ok to.
